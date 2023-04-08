ef4a612fb32cf6ffe23c7fbc380c96b9c853469e
erosman <erosman@users.noreply.github.com>
2023-04-08 20:12:00 +0330

    Delete iframe.js

--- content/iframe.js
+++
@@ -1,3 +0,0 @@
-document.body.classList.toggle('dark', localStorage.getItem('dark') === 'true');
-document.querySelector('input[type="range"]')?.addEventListener('input', e =>
-  e.target.nextElementSibling.textContent = e.target.value);
